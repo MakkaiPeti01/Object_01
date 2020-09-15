d59bf8ec2876d69b1c4d906996e7d3b3ab155d00
MakkaiPeti01 <PetyaPower@gmail.com>
2020-09-15 08:27:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_01
+{
+    class Ember
+    {
+        string nev;
+        int eletkor;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ember pista = new Ember();
+
+            Console.ReadKey();
+        }
+    }
+}
