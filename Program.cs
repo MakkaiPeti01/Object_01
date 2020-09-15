76aae42c8748c43fb4424c2903cf876741ac6412
MakkaiPeti01 <PetyaPower@gmail.com>
2020-09-15 09:22:12 +0200

    bemutatkozas()

--- Program.cs
+++ Program.cs
@@ -8,15 +8,28 @@ namespace Object_01
 {
     class Ember
     {
-        string nev;
-        int eletkor;
+        public string nev;
+        public int eletkor;
+        public string bemutatkozas()
+        {
+            string s= $"{nev} vagyok {eletkor} éves";
+            return s;
+            //Console.WriteLine("{0} vagyok {1} éves", nev, eletkor);
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
             Ember pista = new Ember();
-
+            Ember eva = new Ember();
+            eva.eletkor = 18;
+            eva.nev = "Nagy Eva";
+            pista.eletkor = 26;
+            pista.eletkor++;
+            pista.nev = "Tóth István";       
+            Console.WriteLine(pista.bemutatkozas());
+            Console.WriteLine(eva.bemutatkozas());
             Console.ReadKey();
         }
     }
